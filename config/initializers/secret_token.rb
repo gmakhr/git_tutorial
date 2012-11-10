5b441277229dd661a61f33d255816cb02903deaa
akhrgm <akihiro@lion.local>
2012-11-11 05:29:43 +0900

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GitTutorial::Application.config.secret_token = '6f3ed8295f865cc6e3da890af01a2fcf8461d31836463bd43d6e9a58d46b4a615b821c02e97bcc73d1b80025c633f874c92781b9ccc7386e5ff51cf62a4c56e4'
